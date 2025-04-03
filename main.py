44d7d2cd60eb456fb7f9047dba06cfa1d353b8a9
Kai <ttkhai93@gmail.com>
2025-04-03 12:55:48 +0700

    itertools

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from collections import Counter, defaultdict, deque
+from itertools import product, permutations, combinations, combinations_with_replacement, accumulate, groupby, count, cycle, repeat
 
 if __name__ == '__main__':
     test = "aabbbcc"
@@ -39,3 +40,28 @@ if __name__ == '__main__':
     print(q)
     q.rotate(-1)
     print(q)
+
+    a = (1, 2)
+    b = (3,)
+    print(list(product(a, b)))
+
+
+    num = (1,2,3)
+    print(list(permutations(num, 3)))
+    print(list(combinations(num, 2)))
+    print(list(combinations_with_replacement(num, 2)))
+
+    num = (1,3,2)
+
+    print(list(accumulate(num, func=max)))
+
+    for i in count(1):
+        print(i)
+        if i == 15:
+            break
+
+    for i in cycle(num):
+        print(i)
+
+    for i in repeat(1):
+        print(i)
